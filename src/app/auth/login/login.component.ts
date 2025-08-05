20c3f89f72f7e62a3d49e31038a1bb23d9813212
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-05 16:48:49 +0530

    valueChanges and setValue are important features if we want to implement storing the state of form in localStorage, we can use setTimeout to make sure the function runs after the form-template is initialised well with all its controls

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -21,6 +21,15 @@ export class LoginComponent {
 
   constructor() {
     afterNextRender(() => {
+      const savedForm = window.localStorage.getItem('saved-login-form');
+      if (savedForm) {
+        const loadedFormData = JSON.parse(savedForm);
+        const savedEmail = loadedFormData.email;
+        setTimeout(() => {
+          this.form().controls['email'].setValue(savedEmail);
+        }, 1);
+      }
+
       const subscription = this.form()
         .valueChanges?.pipe(debounceTime(500))
         .subscribe({
