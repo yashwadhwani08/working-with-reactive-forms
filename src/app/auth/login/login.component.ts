e8b49e4784ad8b15214ad114ef1c118c741722a1
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-05 16:39:25 +0530

    added the code to save the information entered by user to browser local-storage that can be later to used to retrieve form values

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
+import {
+  afterNextRender,
+  Component,
+  DestroyRef,
+  inject,
+  viewChild,
+} from '@angular/core';
 import { FormsModule, NgForm } from '@angular/forms';
+import { debounceTime } from 'rxjs';
 
 @Component({
   selector: 'app-login',
@@ -9,13 +16,33 @@ import { FormsModule, NgForm } from '@angular/forms';
   styleUrl: './login.component.css',
 })
 export class LoginComponent {
+  private form = viewChild.required<NgForm>('form');
+  private destroyRef = inject(DestroyRef);
+
+  constructor() {
+    afterNextRender(() => {
+      const subscription = this.form()
+        .valueChanges?.pipe(debounceTime(500))
+        .subscribe({
+          next: (value) =>
+            window.localStorage.setItem(
+              'saved-login-form',
+              JSON.stringify({ email: value.email })
+            ),
+        });
+      this.destroyRef.onDestroy(() => subscription?.unsubscribe());
+    });
+  }
+
   onSubmit(formData: NgForm) {
-    if(formData.form.invalid){
+    if (formData.form.invalid) {
       return;
     }
     const enteredEmail = formData.form.value.email;
     const eneteredPassword = formData.form.value.password;
 
     console.log(enteredEmail, eneteredPassword);
+    formData.form.reset();
+    // clear the input values, it will also reset internally managed information about the form
   }
 }
