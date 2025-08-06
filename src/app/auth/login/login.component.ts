563d8aed7d21cc49a9448742c3210685e7eb59b3
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-06 10:36:46 +0530

    since we are using reactive forms, forms created in TS code, we could take advantage of the fact that we could use ngOnInit(component initialisation) instead of ngAfterNextRender(template initialisation) and also we could load data from local-storage right when the file is getting executed in the browser (detached from component lifecycle) instead of doing it in ngOnInit

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, DestroyRef, inject, OnInit } from '@angular/core';
 import {
   AbstractControl,
   FormControl,
@@ -6,7 +6,7 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
-import { of } from 'rxjs';
+import { debounceTime, of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
@@ -21,6 +21,14 @@ function emailIsUnique(control: AbstractControl) {
   }
   return of({ notUnique: true });
 }
+
+let initialEmailValue = '';
+const savedForm = window.localStorage.getItem('saved-login-form');
+
+if (savedForm) {
+  const loadedForm = JSON.parse(savedForm);
+  initialEmailValue = loadedForm.email;
+}
 @Component({
   selector: 'app-login',
   standalone: true,
@@ -28,9 +36,11 @@ function emailIsUnique(control: AbstractControl) {
   templateUrl: './login.component.html',
   styleUrl: './login.component.css',
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
+  private destroyRef = inject(DestroyRef);
+
   form = new FormGroup({
-    email: new FormControl('', {
+    email: new FormControl(initialEmailValue, {
       validators: [Validators.email, Validators.required],
       asyncValidators: [emailIsUnique],
     }),
@@ -42,6 +52,30 @@ export class LoginComponent {
       ],
     }),
   });
+
+  ngOnInit() {
+    // const savedForm = window.localStorage.getItem('saved-login-form');
+
+    // if (savedForm) {
+    //   const loadedForm = JSON.parse(savedForm);
+    //   // this.form.controls.email.setValue(loadedForm.email);
+    //   this.form.patchValue({
+    //     email: loadedForm.email,
+    //   });
+    // }
+    const subscription = this.form.valueChanges
+      .pipe(debounceTime(500))
+      .subscribe({
+        next: (value) => {
+          window.localStorage.setItem(
+            'saved-login-form',
+            JSON.stringify({ email: value.email })
+          );
+        },
+      });
+
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 
   get emailIsInvalid() {
     return (
