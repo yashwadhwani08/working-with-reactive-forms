107583736ae5835d23e6d526d101b212a842f4f3
Yash Wadhwani <yashwadhwani08@gmail.com>
2025-08-06 09:50:05 +0530

    created acuston async validator, that requires an observable to be returned, we can create an observable instantly for the value we want to emit, using 'of' from rxjs

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -6,12 +6,20 @@ import {
   ReactiveFormsModule,
   Validators,
 } from '@angular/forms';
+import { of } from 'rxjs';
 
 function mustContainQuestionMark(control: AbstractControl) {
   if (control.value.includes('?')) {
     return null;
   }
   return { doesNotContainQuestionMark: true };
+}
+
+function emailIsUnique(control: AbstractControl) {
+  if (control.value !== 'test@example.com') {
+    return of(null); // of returns an observable instantly for the value passed, async validators need an observable
+  }
+  return of({ notUnique: true });
 }
 @Component({
   selector: 'app-login',
@@ -24,9 +32,14 @@ export class LoginComponent {
   form = new FormGroup({
     email: new FormControl('', {
       validators: [Validators.email, Validators.required],
+      asyncValidators: [emailIsUnique],
     }),
     password: new FormControl('', {
-      validators: [Validators.required, Validators.minLength(6), mustContainQuestionMark],
+      validators: [
+        Validators.required,
+        Validators.minLength(6),
+        mustContainQuestionMark,
+      ],
     }),
   });
 
